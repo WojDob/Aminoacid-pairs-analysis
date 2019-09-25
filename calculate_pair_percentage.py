3996fc567a79894ac2d9c63a1a60eb56cb24e9fc
Wojdob <wojciech.dobrychlop@gmail.com>
2019-09-25 10:59:27 +0200

    Finished script

--- calculate_pair_percentage.py
+++ calculate_pair_percentage.py
@@ -2,30 +2,37 @@
 from Bio import SeqIO
 import sys
 
-combinations = ['AC', 'AD',
-'AE', 'AF', 'AG', 'AH', 'AI', 'AK', 'AL', 'AM', 'AN', 'AP', 'AQ',
-'AR', 'AS', 'AT', 'AV', 'AW', 'AY', 'CD', 'CE', 'CF', 'CG', 'CH', 'CI', 'CK',
-'CL', 'CM', 'CN', 'CP', 'CQ', 'CR', 'CS', 'CT', 'CV', 'CW', 'CY', 'DE', 'DF',
-'DG', 'DH', 'DI', 'DK', 'DL', 'DM', 'DN', 'DP', 'DQ', 'DR', 'DS', 'DT', 'DV',
-'DW', 'DY', 'EF', 'EG', 'EH', 'EI', 'EK', 'EL', 'EM', 'EN', 'EP', 'EQ', 'ER',
-'ES', 'ET', 'EV', 'EW', 'EY', 'FG', 'FH', 'FI', 'FK', 'FL', 'FM', 'FN', 'FP',
-'FQ', 'FR', 'FS', 'FT', 'FV', 'FW', 'FY', 'GH', 'GI', 'GK', 'GL', 'GM', 'GN',
-'GP', 'GQ', 'GR', 'GS', 'GT', 'GV', 'GW', 'GY', 'HI', 'HK', 'HL', 'HM', 'HN',
-'HP', 'HQ', 'HR', 'HS', 'HT', 'HV', 'HW', 'HY', 'IK', 'IL', 'IM', 'IN', 'IP',
-'IQ', 'IR', 'IS', 'IT', 'IV', 'IW', 'IY', 'KL', 'KM', 'KN', 'KP', 'KQ', 'KR',
-'KS', 'KT', 'KV', 'KW', 'KY', 'LM', 'LN', 'LP', 'LQ', 'LR', 'LS', 'LT', 'LV',
-'LW', 'LY', 'MN', 'MP', 'MQ', 'MR', 'MS', 'MT', 'MV', 'MW', 'MY', 'NP', 'NQ',
-'NR', 'NS', 'NT', 'NV', 'NW', 'NY', 'PQ', 'PR', 'PS', 'PT', 'PV', 'PW', 'PY',
-'QR', 'QS', 'QT', 'QV', 'QW', 'QY', 'RS', 'RT', 'RV', 'RW', 'RY', 'ST', 'SV',
-'SW', 'SY', 'TV', 'TW', 'TY', 'VW', 'VY', 'WY']
+combinations = ['AA', 'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI', 'AK', 'AL',
+'AM', 'AN', 'AP', 'AQ', 'AR', 'AS', 'AT', 'AV', 'AW', 'AY', 'CC', 'CD', 'CE',
+'CF', 'CG', 'CH', 'CI', 'CK', 'CL', 'CM', 'CN', 'CP', 'CQ', 'CR', 'CS', 'CT',
+'CV', 'CW', 'CY', 'DD', 'DE', 'DF', 'DG', 'DH', 'DI', 'DK', 'DL', 'DM', 'DN',
+'DP', 'DQ', 'DR', 'DS', 'DT', 'DV', 'DW', 'DY', 'EE', 'EF', 'EG', 'EH', 'EI',
+'EK', 'EL', 'EM', 'EN', 'EP', 'EQ', 'ER', 'ES', 'ET', 'EV', 'EW', 'EY', 'FF',
+'FG', 'FH', 'FI', 'FK', 'FL', 'FM', 'FN', 'FP', 'FQ', 'FR', 'FS', 'FT', 'FV',
+'FW', 'FY', 'GG', 'GH', 'GI', 'GK', 'GL', 'GM', 'GN', 'GP', 'GQ', 'GR', 'GS',
+'GT', 'GV', 'GW', 'GY', 'HH', 'HI', 'HK', 'HL', 'HM', 'HN', 'HP', 'HQ', 'HR',
+'HS', 'HT', 'HV', 'HW', 'HY', 'II', 'IK', 'IL', 'IM', 'IN', 'IP', 'IQ', 'IR',
+'IS', 'IT', 'IV', 'IW', 'IY', 'KK', 'KL', 'KM', 'KN', 'KP', 'KQ', 'KR', 'KS',
+'KT', 'KV', 'KW', 'KY', 'LL', 'LM', 'LN', 'LP', 'LQ', 'LR', 'LS', 'LT', 'LV',
+'LW', 'LY', 'MM', 'MN', 'MP', 'MQ', 'MR', 'MS', 'MT', 'MV', 'MW', 'MY', 'NN',
+'NP', 'NQ', 'NR', 'NS', 'NT', 'NV', 'NW', 'NY', 'PP', 'PQ', 'PR', 'PS', 'PT',
+'PV', 'PW', 'PY', 'QQ', 'QR', 'QS', 'QT', 'QV', 'QW', 'QY', 'RR', 'RS', 'RT',
+'RV', 'RW', 'RY', 'SS', 'ST', 'SV', 'SW', 'SY', 'TT', 'TV', 'TW', 'TY', 'VV',
+'VW', 'VY', 'WW', 'WY', 'YY']
 
-comb = combinations[1]
+for comb in combinations:
+    print("Calculating {}-{}".format(comb,comb[::-1]))
+    f = open("output/{}-{}_percentage.txt".format(comb,comb[::-1]),"w+")
+    output = list()
+    for rec in SeqIO.parse(sys.argv[1], "fasta"):
+        positions = list()
+        for i in range(len(rec.seq)-1):
+            if rec.seq[i]+rec.seq[i+1] in [comb,comb[::-1]] :
+                positions.append(i)
+                positions.append(i+1)
+        percentage = len(set(positions))/len(rec.seq) *100
+        output.append((rec.id,percentage))
 
-for rec in SeqIO.parse(sys.argv[1], "fasta"):
-    print(rec.seq)
-    positions = list()
-    for i in range(len(rec.seq)-1):
-        if rec.seq[i]+rec.seq[i+1] in [comb,comb[::-1]] :
-            positions.append(i)
-            positions.append(i+1)
-    print(len(set(positions))/len(rec.seq))
+    output.sort(key=lambda tup: tup[1], reverse = True)
+    f.write('\n'.join('{} {}'.format(x[0],x[1]) for x in output))
+    f.close()
