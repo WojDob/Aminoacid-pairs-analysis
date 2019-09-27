e3556282f8286055672179cbb1975a83b1ed0d3a
Wojdob <wojciech.dobrychlop@gmail.com>
2019-09-27 13:03:32 +0200

    Added function to calculate pair frequency

--- calculate_pair_percentage.py
+++ calculate_pair_percentage.py
@@ -20,9 +20,37 @@ combinations = ['AA', 'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI', 'AK', 'AL',
 'RV', 'RW', 'RY', 'SS', 'ST', 'SV', 'SW', 'SY', 'TT', 'TV', 'TW', 'TY', 'VV',
 'VW', 'VY', 'WW', 'WY', 'YY']
 
+frequency = {
+    'A':0.082,
+    'R':0.055,
+    'N':0.04,
+    'D':0.054,
+    'C':0.013,
+    'Q':0.039,
+    'E':0.067,
+    'G':0.07,
+    'H':0.022,
+    'I':0.059,
+    'L':0.096,
+    'K':0.058,
+    'M':0.024,
+    'F':0.038,
+    'P':0.047,
+    'S':0.066,
+    'T':0.053,
+    'W':0.01,
+    'Y':0.029,
+    'V':0.068
+}
+
+def pairFrequency(comb):
+    a = list(comb)
+    return frequency[a[0]]*frequency[a[1]]*2
+
+
 for comb in combinations:
     print("Calculating {}-{}".format(comb,comb[::-1]))
-    f = open("output/{}-{}_percentage.txt".format(comb,comb[::-1]),"w+")
+    f = open("output/{}-{}_ratio.txt".format(comb,comb[::-1]),"w+")
     output = list()
     for rec in SeqIO.parse(sys.argv[1], "fasta"):
         positions = list()
