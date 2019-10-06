3333cdb3b9116e04ace7c126e8970dfa9ed4d384
Wojciech Dobrychłop <wojciech.dobrychlop@gmail.com>
2019-10-07 00:13:26 +0200

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,23 +1,28 @@
 import matplotlib.pyplot as plt
 import json
 import os
-
-#kind of broken but not really, move it to output folder for it to even start
+import numpy as np
 
 for subdir, dirs, files in os.walk('./'):
     for file in files:
         with open(file, 'r') as f:
-            # try:
-            motif = json.load(f)
-            print(motif['motif'])
-            ratios= [ x['ratio'] for x in motif['proteins']]
+            try:
+                motif = json.load(f)
+                print(motif['motif'])
+                ratios= [ x['ratio'] for x in motif['proteins'] if x['ratio']>1]
 
-            plt.plot(ratios)
-            plt.xlabel('Proteins')
-            plt.ylabel('Ratio')
-            plt.title('{} plot'.format(motif['motif']))
-            plt.savefig('./plots/{}.png'.format(motif['motif'][0]),bbox_inches='tight')
-            plt.clf()
-            f.close()
-            # except:
-            #     print("error {}".format(motif['motif']))
+                plt.hist(ratios, bins = 50)
+                plt.xlabel('Ratio')
+                plt.ylabel('Number of proteins')
+                locs, labels = plt.yticks()            
+                plt.yticks(np.arange(0, 5001, step=250))  
+                # locs, labels = plt.xticks()            
+                # plt.xticks(np.arange(0, 25, step=1))            
+                plt.title('{}'.format(motif['motif']))
+                figure = plt.gcf() # get current figure
+                figure.set_size_inches(15, 10)
+                plt.savefig('./plots/{}.png'.format(motif['motif'][0]),dpi = 100)
+                plt.clf()
+                f.close()
+            except:
+                print('{} error'.format(motif['motif']))
