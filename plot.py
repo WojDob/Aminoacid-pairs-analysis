33ea98731d69bd016847ea9f0923ed05ff450eb1
Wojdob <wojciech.dobrychlop@gmail.com>
2019-10-06 22:19:14 +0200

    Added plot titles

--- plot.py
+++ plot.py
@@ -2,18 +2,22 @@ import matplotlib.pyplot as plt
 import json
 import os
 
+#kind of broken but not really, move it to output folder for it to even start
+
 for subdir, dirs, files in os.walk('./'):
     for file in files:
         with open(file, 'r') as f:
-            try:
-                motif = json.load(f)
-                print(motif['motif'])
-                ratios= [ x['ratio'] for x in motif['proteins']]
+            # try:
+            motif = json.load(f)
+            print(motif['motif'])
+            ratios= [ x['ratio'] for x in motif['proteins']]
 
-                plt.plot(ratios)
-                plt.savefig('./plots/{}.png'.format(motif['motif'][0]),bbox_inches='tight')
-                plt.clf()
-                f.close()
-            except:
-                print("error {}".format(motif['motif']))
-                pass
+            plt.plot(ratios)
+            plt.xlabel('Proteins')
+            plt.ylabel('Ratio')
+            plt.title('{} plot'.format(motif['motif']))
+            plt.savefig('./plots/{}.png'.format(motif['motif'][0]),bbox_inches='tight')
+            plt.clf()
+            f.close()
+            # except:
+            #     print("error {}".format(motif['motif']))
