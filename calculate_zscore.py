0d0c55534d43c11274d3df87bf89b16f9b0d0566
Wojdob <wojciech.dobrychlop@gmail.com>
2019-10-23 13:56:22 +0200

    Added plot saving

--- calculate_zscore.py
+++ calculate_zscore.py
@@ -3,94 +3,91 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 import json
-
-frequency = {
-    'A':0.082,
-    'R':0.055,
-    'N':0.04,
-    'D':0.054,
-    'C':0.013,
-    'Q':0.039,
-    'E':0.067,
-    'G':0.07,
-    'H':0.022,
-    'I':0.059,
-    'L':0.096,
-    'K':0.058,
-    'M':0.024,
-    'F':0.038,
-    'P':0.047,
-    'S':0.066,
-    'T':0.053,
-    'W':0.01,
-    'Y':0.029,
-    'V':0.068
-}
+import utils
 
 
 def pairFrequency(comb):
     a = list(comb)
-    return frequency[a[0]]*frequency[a[1]]*2*100
+    return utils.frequency[a[0]]*utils.frequency[a[1]]*2*100
 
 def getDataList(key):
     return [protein[key] for protein in zScoreOutput['proteins']]
 
-comb = 'WG'
-
-zScoreOutput = {
-    'motif': [comb,comb[::-1]],
-    'averageOccurence' : -1,
-    'standardDeviation' : -1,
-    'proteins':[]
-}
+combinations = ['WG']
 
 
-countList = list()
-for rec in SeqIO.parse("C:\\Users\\wojci\\Desktop\\github\\Aminoacid-pairs-analysis\\data\\Arabidopsis_filtered.fa", "fasta"):
 
-    protein = {
-        'id':rec.id,
-        'length': len(rec.seq),
-        'count':-1,
-        'ratio':-1,
-        'zscore':-1,
+for comb in combinations:
+
+    zScoreOutput = {
+        'motif': [comb,comb[::-1]],
+        'averageOccurence' : -1,
+        'standardDeviation' : -1,
+        'proteins':[]
     }
+    print("Calculating {}".format(zScoreOutput['motif']))
+    countList = list()
+    #for every protein record in data file
+    for rec in SeqIO.parse(sys.argv[1], "fasta"):
+
+        protein = {
+            'id':rec.id,
+            'length': len(rec.seq),
+            'count':-1,
+            'ratio':-1,
+            'zscore':-1,
+        }
+
+        #calculate ratio of expected occurence and actual
+        positions = list()
+        for i in range(len(rec.seq)-1):
+            if rec.seq[i]+rec.seq[i+1] in [comb,comb[::-1]] :
+                positions.append(i)
+                positions.append(i+1)
+        percentage = len(set(positions))/len(rec.seq) *100
+        protein['ratio'] = percentage / pairFrequency(comb)
+
+        #count instances of motif
+        recCombCount = rec.seq.count(comb) + rec.seq.count(comb[::-1])
+        protein['count'] = recCombCount
+        countList.append(recCombCount)
+
+        #add the protein to the list
+        zScoreOutput['proteins'].append(protein)
+
+    zScoreOutput["averageOccurence"] = np.mean(countList)
+    zScoreOutput["standardDeviation"] = np.std(countList)
+
+    #calculate zscore of each protein
+    for rec in zScoreOutput['proteins']:
+        rec['zscore'] =(rec['count'] - zScoreOutput['averageOccurence'])/zScoreOutput["standardDeviation"]
+
+    zScoresList = getDataList('zscore')
+    ratioList = getDataList('ratio')
+    lengthList = getDataList('length')
+
+    plt.hist(zScoresList,bins = 50)
+    plt.title(zScoreOutput['motif'])
+    figure = plt.gcf()
+    figure.set_size_inches(15, 10)
+    plt.savefig('./zscore_plots/zscore_histograms/{}-{}_zs_hist.png'.format(zScoreOutput['motif'][0],zScoreOutput['motif'][1]),dpi = 100)
+    plt.clf()
 
 
-    #calculate ratio of expected occurence and actual
-    positions = list()
-    for i in range(len(rec.seq)-1):
-        if rec.seq[i]+rec.seq[i+1] in [comb,comb[::-1]] :
-            positions.append(i)
-            positions.append(i+1)
-    percentage = len(set(positions))/len(rec.seq) *100
-    protein['ratio'] = percentage / pairFrequency(comb)
+    plt.scatter(ratioList,lengthList,alpha=0.1)
+    plt.xlabel('Ratio')
+    plt.ylabel('Length of sequence')
+    plt.title(zScoreOutput['motif'])
+    figure = plt.gcf()
+    figure.set_size_inches(15, 10)
+    plt.savefig('./zscore_plots/ratio-length_scatterplots/{}-{}_r-l_scttr.png'.format(zScoreOutput['motif'][0],zScoreOutput['motif'][1]),dpi = 100)
+    plt.clf()
 
-    #count instances of motif
-    recCombCount = rec.seq.count(comb) + rec.seq.count(comb[::-1])
-    protein['count'] = recCombCount
-    countList.append(recCombCount)
-    #add the protein to the list
-    zScoreOutput['proteins'].append(protein)
-
-zScoreOutput["averageOccurence"] = np.mean(countList)
-zScoreOutput["standardDeviation"] = np.std(countList)
-
-#calculate zscore of each protein
-for rec in zScoreOutput['proteins']:
-    rec['zscore'] =(rec['count'] - zScoreOutput['averageOccurence'])/zScoreOutput["standardDeviation"]
-
-
-
-plt.hist([protein['zscore'] for protein in zScoreOutput['proteins']],bins = 50)
-plt.show()
-
-plt.scatter([protein['ratio'] for protein in zScoreOutput['proteins']],[protein['length'] for protein in zScoreOutput['proteins']],alpha=0.1)
-plt.xlabel('Ratio')
-plt.ylabel('Length of sequence')
-plt.show()
-
-plt.scatter(getDataList('ratio'),getDataList('zscore'),alpha=0.1)
-plt.xlabel('Ratio')
-plt.ylabel('Z-score')
-plt.show()
+    plt.scatter(ratioList,zScoresList,alpha=0.1)
+    plt.xlabel('Ratio')
+    plt.ylabel('Z-score')
+    plt.title(zScoreOutput['motif'])
+    figure = plt.gcf()
+    figure.set_size_inches(15, 10)
+    plt.savefig('./zscore_plots/ratio-zscore_scatterplots/{}-{}_r-zs_scttr.png'.format(zScoreOutput['motif'][0],zScoreOutput['motif'][1]),dpi = 100)
+    plt.clf()
