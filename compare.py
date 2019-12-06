4acf8f81e000557ea23dbcfb1f59f9c6538cec53
Wojdob <wojciech.dobrychlop@gmail.com>
2019-12-06 16:53:58 +0100

    added alignments

--- compare.py
+++ compare.py
@@ -1,4 +1,5 @@
 from Bio import SeqIO
+import os
 import json
 import csv
 
@@ -37,21 +38,29 @@ for comb in combinations:
     print(comb)
     with open("best_zscores/{}-{}_bz.json".format(comb,comb[::-1]), 'r') as f:
         best = json.load(f)
+
+    orthologs ={}
+    for protein in best['proteins']:
+        orthologs[protein["id"]] = []
+
+
     print("A.thaliana id \t zscore \t\t B.rapa id \t length  count   zscore")
     for protein in best['proteins']:
         for row in brapa:
             if row[0] == protein["id"] and row[1]!="":
                 for rec in SeqIO.parse("data/Brassica_rapa.Brapa_1.0.pep.all.fa", "fasta"):
                     if rec.id == row[1]:
+                        orthologs[protein["id"]].append(rec)
                         count = rec.seq.count(comb) + rec.seq.count(comb[::-1])
                         print("{} \t {} \t {} \t {} \t {} \t {}".format(protein["id"],protein["zscore"],rec.id,len(rec.seq),count,(count - best["averageOccurence"])/best["standardDeviation"]))
-
+    
     print("A.thaliana id \t zscore \t\t G.max id \t length  count   zscore")
     for protein in best['proteins']:
         for row in gmax:
             if row[0] == protein["id"] and row[1]!="":
                 for rec in SeqIO.parse("data/Glycine_max.Glycine_max_v2.1.pep.all.fa", "fasta"):
                     if rec.id == row[1]:
+                        orthologs[protein["id"]].append(rec)
                         count = rec.seq.count(comb) + rec.seq.count(comb[::-1])
                         print("{} \t {} \t {} \t {} \t {} \t {}".format(protein["id"],protein["zscore"],rec.id,len(rec.seq),count,(count - best["averageOccurence"])/best["standardDeviation"]))
     print("A.thaliana id \t zscore \t\t O.indica id \t length  count   zscore")
@@ -60,6 +69,7 @@ for comb in combinations:
             if row[0] == protein["id"] and row[1]!="":
                 for rec in SeqIO.parse("data/Oryza_indica.ASM465v1.pep.all.fa", "fasta"):
                     if rec.id == row[1]:
+                        orthologs[protein["id"]].append(rec)
                         count = rec.seq.count(comb) + rec.seq.count(comb[::-1])
                         print("{} \t {} \t {} \t {} \t {} \t {}".format(protein["id"],protein["zscore"],rec.id,len(rec.seq),count,(count - best["averageOccurence"])/best["standardDeviation"]))
     print("A.thaliana id \t zscore \t\t O.japonica id \t length  count   zscore")
@@ -68,6 +78,7 @@ for comb in combinations:
             if row[0] == protein["id"] and row[1]!="":
                 for rec in SeqIO.parse("data/Oryza_sativa.IRGSP-1.0.pep.all.fa", "fasta"):
                     if rec.id == row[1]:
+                        orthologs[protein["id"]].append(rec)
                         count = rec.seq.count(comb) + rec.seq.count(comb[::-1])
                         print("{} \t {} \t {} \t {} \t {} \t {}".format(protein["id"],protein["zscore"],rec.id,len(rec.seq),count,(count - best["averageOccurence"])/best["standardDeviation"]))
     print("A.thaliana id \t zscore \t\t M.truncatula id \t length  count   zscore")
@@ -76,6 +87,7 @@ for comb in combinations:
             if row[0] == protein["id"] and row[1]!="":
                 for rec in SeqIO.parse("data/Medicago_truncatula.MedtrA17_4.0.pep.all.fa", "fasta"):
                     if rec.id == row[1]:
+                        orthologs[protein["id"]].append(rec)
                         count = rec.seq.count(comb) + rec.seq.count(comb[::-1])
                         print("{} \t {} \t {} \t {} \t {} \t {}".format(protein["id"],protein["zscore"],rec.id,len(rec.seq),count,(count - best["averageOccurence"])/best["standardDeviation"]))
     print("A.thaliana id \t zscore \t\t P.patens id \t length  count   zscore")
@@ -84,6 +96,7 @@ for comb in combinations:
             if row[0] == protein["id"] and row[1]!="":
                 for rec in SeqIO.parse("data/Physcomitrella_patens.Phypa_V3.pep.all.fa", "fasta"):
                     if rec.id == row[1]:
+                        orthologs[protein["id"]].append(rec)
                         count = rec.seq.count(comb) + rec.seq.count(comb[::-1])
                         print("{} \t {} \t {} \t {} \t {} \t {}".format(protein["id"],protein["zscore"],rec.id,len(rec.seq),count,(count - best["averageOccurence"])/best["standardDeviation"]))
     print("A.thaliana id \t zscore \t\t V.vinifera id \t length  count   zscore")
@@ -92,5 +105,19 @@ for comb in combinations:
             if row[0] == protein["id"] and row[1]!="":
                 for rec in SeqIO.parse("data/Vitis_vinifera.12X.pep.all.fa", "fasta"):
                     if rec.id == row[1]:
+                        orthologs[protein["id"]].append(rec)
                         count = rec.seq.count(comb) + rec.seq.count(comb[::-1])
                         print("{} \t {} \t {} \t {} \t {} \t {}".format(protein["id"],protein["zscore"],rec.id,len(rec.seq),count,(count - best["averageOccurence"])/best["standardDeviation"]))
+
+  
+
+    for protein in orthologs:
+        f = open("temp.txt","w")
+        for rec in SeqIO.parse("data/Arabidopsis_filtered.fa", "fasta"): #zrobic to optymalnie, przy robieniu slownika
+            if rec.id == protein:
+                SeqIO.write(rec, f, "fasta")
+        for ortholog in orthologs[protein]:
+            SeqIO.write(ortholog, f, "fasta")
+
+        f.close()
+        os.system("mafft --auto --clustalout temp.txt > alignments/{}_{}-{}.txt".format(comb,comb[::-1],protein))
