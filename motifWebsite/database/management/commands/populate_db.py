d39d2e5e750afba52e2bbabb6149a62b4730e3de
Wojdob <wojciech.dobrychlop@gmail.com>
2020-05-02 17:06:53 +0200

    Implemented database and added records of AA motif

---
+++ motifWebsite/database/management/commands/populate_db.py
@@ -0,0 +1,73 @@
+from django.core.management.base import BaseCommand
+from database.models import *
+from Bio import SeqIO
+import json
+
+
+class Command(BaseCommand):
+	args = 'idontknow'
+	help = 'sorry no help here'
+
+	def _create_organisms(self):
+		athaliana = Organism(name="Arabidopsis thaliana")
+		athaliana.save()
+
+	def _create_motifs(self):
+		for comb in combinations:
+			motif = Motif(motif=comb)
+			motif.save()
+
+	def _create_proteins(self):
+		athaliana = Organism.objects.get(name="Arabidopsis thaliana")
+
+		for rec in SeqIO.parse("Arabidopsis_filtered.fa", "fasta"):
+			protein = Protein(
+				organism = athaliana,
+				proteinId = rec.id,
+				description = rec.description,
+				length = len(rec.seq),
+				sequence = rec.seq
+				)
+			protein.save()
+
+	def _create_results(self):
+		for comb in combinations:
+			with open("fullresults/{}-{}.json".format(comb,comb[::-1]), 'r') as f:
+				jsonFile = json.load(f)
+
+			for prot in jsonFile["proteins"]:
+				result = Result(
+					motif = Motif.objects.get(motif=comb),
+					protein = Protein.objects.get(proteinId = prot["id"]),
+					count = prot["count"],
+					zscore = prot["zscore"],
+					ratio = prot["ratio"],
+					pvalue = prot["pvalue"]
+
+					)
+				result.save()
+
+	def handle(self, *args, **options):
+		self._create_results()
+
+
+
+combinations2 = ['AA']
+
+combinations = ['AA', 'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI', 'AK', 'AL',
+'AM', 'AN', 'AP', 'AQ', 'AR', 'AS', 'AT', 'AV', 'AW', 'AY', 'CC', 'CD', 'CE',
+'CF', 'CG', 'CH', 'CI', 'CK', 'CL', 'CM', 'CN', 'CP', 'CQ', 'CR', 'CS', 'CT',
+'CV', 'CW', 'CY', 'DD', 'DE', 'DF', 'DG', 'DH', 'DI', 'DK', 'DL', 'DM', 'DN',
+'DP', 'DQ', 'DR', 'DS', 'DT', 'DV', 'DW', 'DY', 'EE', 'EF', 'EG', 'EH', 'EI',
+'EK', 'EL', 'EM', 'EN', 'EP', 'EQ', 'ER', 'ES', 'ET', 'EV', 'EW', 'EY', 'FF',
+'FG', 'FH', 'FI', 'FK', 'FL', 'FM', 'FN', 'FP', 'FQ', 'FR', 'FS', 'FT', 'FV',
+'FW', 'FY', 'GG', 'GH', 'GI', 'GK', 'GL', 'GM', 'GN', 'GP', 'GQ', 'GR', 'GS',
+'GT', 'GV', 'GW', 'GY', 'HH', 'HI', 'HK', 'HL', 'HM', 'HN', 'HP', 'HQ', 'HR',
+'HS', 'HT', 'HV', 'HW', 'HY', 'II', 'IK', 'IL', 'IM', 'IN', 'IP', 'IQ', 'IR',
+'IS', 'IT', 'IV', 'IW', 'IY', 'KK', 'KL', 'KM', 'KN', 'KP', 'KQ', 'KR', 'KS',
+'KT', 'KV', 'KW', 'KY', 'LL', 'LM', 'LN', 'LP', 'LQ', 'LR', 'LS', 'LT', 'LV',
+'LW', 'LY', 'MM', 'MN', 'MP', 'MQ', 'MR', 'MS', 'MT', 'MV', 'MW', 'MY', 'NN',
+'NP', 'NQ', 'NR', 'NS', 'NT', 'NV', 'NW', 'NY', 'PP', 'PQ', 'PR', 'PS', 'PT',
+'PV', 'PW', 'PY', 'QQ', 'QR', 'QS', 'QT', 'QV', 'QW', 'QY', 'RR', 'RS', 'RT',
+'RV', 'RW', 'RY', 'SS', 'ST', 'SV', 'SW', 'SY', 'TT', 'TV', 'TW', 'TY', 'VV',
+'VW', 'VY', 'WW', 'WY', 'YY']
--- motifWebsite/database/models.py
+++ motifWebsite/database/models.py
@@ -3,22 +3,34 @@ from django.db import models
 # Create your models here.
 
 class Organism(models.Model):
-	name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
 
 class Motif(models.Model):
-	motif = models.CharField(max_length=2)
+    motif = models.CharField(max_length=2)
 
 class Protein(models.Model):
-	origin = models.ForeignKey(Organism, on_delete=models.CASCADE)
-	proteinId = models.CharField(max_length=20)
-	length = models.PositiveIntegerField()
+    organism = models.ForeignKey(Organism, on_delete=models.CASCADE) # AZ
+    proteinId = models.CharField(max_length=20)
+    description = models.CharField(max_length=1000) # AZ (protein description from FASTA headers)
+    length = models.PositiveIntegerField()
+    sequence = models.TextField() # AZ
 
 class Result(models.Model):
-	motif = models.ForeignKey(Motif, on_delete=models.CASCADE)
-	protein = models.ForeignKey(Protein, on_delete=models.CASCADE)
-	count = models.PositiveIntegerField()
-	zscore = models.FloatField()
-	ratio = models.FloatField()
-	pvalue = models.FloatField()
-	alignment = models.CharField(max_length=1000000)
+    motif = models.ForeignKey(Motif, on_delete=models.CASCADE)
+    protein = models.ForeignKey(Protein, on_delete=models.CASCADE)
+    _motif_positions = models.TextField() # Motif's positions in the protein sequence (e.g., "3,4,6")
+    count = models.PositiveIntegerField()
+    zscore = models.FloatField()
+    ratio = models.FloatField()
+    pvalue = models.FloatField()
+    alignment = models.TextField() # AZ
 
+    @property
+    def motif_positions(self):
+        # >>> results = Results.objects.get(id=1)
+        # >>> print(results._motif_positions)
+        # "3,4,6"
+        # >>> print(results.motif_positions)
+        # [3, 4, 6]
+        return [int(pos) for pos in self._motif_positions.split(',')]
+    
